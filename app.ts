cc4fc700c6636f261948b1ecf5c2d2f69faa92e0
Ryan Xin <xinpf0715@gmail.com>
2021-01-05 09:15:45 +1100

    typescript object

--- app.ts
+++ app.ts
@@ -1,18 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //   throw new Error("Incorrect input.");
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
-}
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: 'ryan',
+  age: 30
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
