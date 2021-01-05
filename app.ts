a1d96905afc1217640b68ce796625feaf1c78184
Ryan Xin <xinpf0715@gmail.com>
2021-01-05 22:14:55 +1100

    unkown & never types

--- app.ts
+++ app.ts
@@ -1,28 +1,15 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-  return;
-  // return num.toString();
+function generateError(message: string, code: number): never {
+  throw {message: message, errorCode: code};
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(20, 10, (result) => {
-  console.log(result);
-  return result;
-})
-
-let combinedValues: (a: number, b: number) => number;
-combinedValues = add;
-// combinedValues = printResult;
-
-// console.log(combinedValues(8, 8));
-
-// printResult(add(5, 12));
-
+generateError('An error occurs', 500);
+// console.log(result);
